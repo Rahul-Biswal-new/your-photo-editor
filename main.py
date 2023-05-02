875748914ac3723ffcfb42ae827cad135f344950
Rahul <rahulbiswalnew@gmail.com>
2023-05-02 11:06:57 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# pip3 install flask opencv-python 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+app.run(debug=True, port=5001)
